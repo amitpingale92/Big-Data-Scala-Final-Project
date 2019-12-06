1b0e323089776a36823b7bede18cd6ee63df1d14
Mayank Gangrade <gangrademayank91@gmail.com>
2019-12-06 14:33:50 -0500

    updates

--- hedge-fund-real-time-analysis/src/main/scala/SparkSessionCreator.scala
+++ hedge-fund-real-time-analysis/src/main/scala/SparkSessionCreator.scala
@@ -3,23 +3,11 @@ import org.apache.spark.sql.SparkSession
 object SparkSessionCreator {
 
   def sparkSessionCreate(): SparkSession = {
-
-    //    SparkSession
-    //      .builder()
-    //      .master("local[*]")
-    //      .appName("hedge-fund-real-time-analysis")
-    //      .getOrCreate()
-
     SparkSession
       .builder()
       .master("local")
       .appName("hedge-fund-real-time-analysis")
-//      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/scaladb.ford")
-//      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/scaladb.ford")
       .getOrCreate()
-
-
-
   }
 
 }
